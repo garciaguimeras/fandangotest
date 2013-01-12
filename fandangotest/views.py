91ce78c1d17cc966f23a770ddd56394ec2890d0d
Noel <garcia.guimeras@gmail.com>
2013-01-12 13:08:47 -0500

    Bugfix

--- fandangotest/views.py
+++ fandangotest/views.py
@@ -3,8 +3,8 @@
 from django.http import HttpResponse
 from fandjango.decorators import facebook_authorization_required
 
-@facebook_authorization_required
+@facebook_authorization_required(permissions=["publish_actions"])
 def canvas(request):
     fb = request.facebook
     user = fb.user
-    return HttpResponse("Hello user: id {0} -- {1} {2} -- {3}".format(user.id, user.first_name, user.last_name, user.language))
+    return HttpResponse("Hello user: id {0} -- {1} {2}".format(user.facebook_id, user.first_name, user.last_name))
