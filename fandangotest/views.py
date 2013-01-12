f63e701c7b7aa6cd41afce862d9f9788e08a05cf
Noel <garcia.guimeras@gmail.com>
2013-01-12 13:29:33 -0500

    Trying to publish on user wall

--- fandangotest/views.py
+++ fandangotest/views.py
@@ -2,9 +2,18 @@
 
 from django.http import HttpResponse
 from fandjango.decorators import facebook_authorization_required
+from facepy import GraphAPI
 
 @facebook_authorization_required(permissions=["publish_actions"])
 def canvas(request):
+
     fb = request.facebook
     user = fb.user
+    access_token = user.oauth_token.token
+
+    graph = GraphAPI(access_token)
+    og_path = "{0}/feed".format(user.facebook_id)
+    message = "TestApp acaba de hacer una publicacion en tu muro"
+    graph.post(path=og_path, og_msg=message)
+    
     return HttpResponse("Hello user: id {0} -- {1} {2}".format(user.facebook_id, user.first_name, user.last_name))
