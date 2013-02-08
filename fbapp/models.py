4b30222d6a52c226f21a366f245f7ce4af56f150
Noel <garcia.guimeras@gmail.com>
2013-02-08 17:48:15 -0500

    Agregando README

--- fbapp/models.py
+++ fbapp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from fandjango.models import User
 
-
+class FacebookUser(models.Model):
+    pass
 
--- fbapp/views.py
+++ fbapp/views.py
@@ -2,7 +2,6 @@
 
 from django.http import HttpResponse
 from fandjango.decorators import facebook_authorization_required
-#from facepy import GraphAPI
 
 def _post_message_on_wall(user, msg):
     fbpath = "{0}/feed".format(user.facebook_id)
@@ -10,24 +9,16 @@ def _post_message_on_wall(user, msg):
 
 @facebook_authorization_required(permissions=["publish_actions"])
 def canvas(request):
-
-    fb = request.facebook
-    user = fb.user
-    #access_token = user.oauth_token.token
-    #graph = GraphAPI(access_token)
-    
+    user = request.facebook.user
     _post_message_on_wall(user, "{0} se acaba de registrar en TestApp".format(user.first_name))
-    
     return HttpResponse("Bienvenido usuario: id {0} -- {1}!".format(user.facebook_id, user.full_name))
     
 def post(request):
-    
     from fandjango.models import User
     users = User.objects.all()
     for user in users:
-        #if user.oauth_token.expired():
-        user.oauth_token.extend()
-        _post_message_on_wall(user, "TestApp ha publicado algo en su muro...")
-        
-    return HttpResponse("Se ha publicado en el muro de {0} usuarios registrados.".format(len(users)))
+        if user.authorized:
+            user.oauth_token.extend()
+            _post_message_on_wall(user, "TestApp ha publicado algo en su muro...")
+    return HttpResponse("Se ha publicado en el muro de los usuarios registrados.")
 
